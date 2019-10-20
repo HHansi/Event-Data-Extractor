75fe06a70f5a85cfb5656f1a3d21d14eb3ba4022
HHansi <h.hansi1991@gmail.com>
2019-10-20 12:31:59 +0100

    007: Added Tweet extraction by hashtag

--- data_extract/tweet_extract.py
+++ data_extract/tweet_extract.py
@@ -1,11 +1,14 @@
 # Created by Hansi at 9/6/2019
 import configparser
-import os
+import csv
+import datetime
+import time
 
-import pandas as pd
 import tweepy
 
-ITERATOR_LENGTH = 2
+ITERATOR_LENGTH = 50
+API_CALL_LIMIT = 180
+TIME_LIMIT = 15 * 60
 
 configParser = configparser.RawConfigParser()
 configFilePath = "../config.txt"
@@ -16,92 +19,54 @@ CONSUMER_SECRET = configParser.get('twitter-dev-configs', 'CONSUMER_SECRET')
 OAUTH_TOKEN = configParser.get('twitter-dev-configs', 'OAUTH_TOKEN')
 OAUTH_TOKEN_SECRET = configParser.get('twitter-dev-configs', 'OAUTH_TOKEN_SECRET')
 
+FULL_DATA_SET_PATH = '../data/full_dataset/'
 
-def extract_by_id(dataset_id):
+
+def get_tweet_by_id(id):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    tweet = api.get_status(id)
+
+    print("Tweet_ID: " + str(id))
+    print("Tweet: " + tweet.text)
+    print("Timestamp: ", tweet.created_at)
+    return tweet
+
+
+def get_tweet_by_hashtag(hashtag, from_date):
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    if dataset_id == 1:
-        # read dataset1 Tweet IDs
-        df_all = pd.read_csv("../data/dataset1/relevant_tweets.tsv", sep='\t', names=['event', 'tweet_id'],
-                             encoding='utf-8')
-        print("data shape: ", df_all.shape)
-        # print("data head: ", df_all.head(10))
+    filename = hashtag.split('#')[1]
 
-        output_file = "../data/full_dataset/dataset" + str(dataset_id) + "/extracted_data.tsv"
+    # Open/Create a file to append data
+    csvFile = open(FULL_DATA_SET_PATH + filename + '.tsv', 'a', newline='', encoding='utf-8')
+    # Use csv Writer
+    csvWriter = csv.writer(csvFile, delimiter='\t')
 
-        # add column names
-        df_extracted = pd.DataFrame(columns=['Event', 'Tweet_Id', 'Tweet', 'HashTags'])
-        df_extracted.to_csv(output_file, sep='\t', mode='a', index=False)
-        df_extracted = df_extracted[:-1]
+    start_time = datetime.datetime.now()
+    i = 0
+    for tweet in tweepy.Cursor(api.search, q=hashtag, count=100,
+                               lang="en",
+                               since=from_date).items():
+        i += 1
+        print(tweet.created_at, tweet.text, get_hashtags(tweet.entities.get('hashtags')))
+        csvWriter.writerow([tweet.id_str, tweet.created_at, tweet.text.encode('utf-8'),
+                            get_hashtags(tweet.entities.get('hashtags')).encode('utf-8'),
+                            get_location(tweet.coordinates),
+                            tweet.user.location.encode('utf-8')])
 
-        i = 0
-        for index, row in df_all.iterrows():
-            i = i + 1
-            tweet_id = row['tweet_id']
-            try:
-                tweet = api.get_status(tweet_id)
-                df_extracted = df_extracted.append({'Event': row['event'], 'Tweet_Id': tweet_id, 'Tweet': tweet.text,
-                                                    'HashTags': get_hashtags(tweet.entities.get('hashtags'))},
-                                                   ignore_index=True)
-            except tweepy.TweepError:
-                print('Error occurred while retrieving Tweet: ' + str(tweet_id))
-                df_extracted = df_extracted.append({'Event': row['event'], 'Tweet_Id': tweet_id, 'Tweet': 'NA',
-                                                    'HashTags': 'NA'}, ignore_index=True)
+        # print('Sleeping for (seconds) : 1')
+        time.sleep(1)
 
-            # save to file when ITERATOR_LENGTH found
-            if i == ITERATOR_LENGTH:
-                df_extracted.to_csv(output_file, sep='\t', mode='a', index=False,
-                                    header=False, encoding='utf-8')
-                i = 0
-                df_extracted = df_extracted[:-ITERATOR_LENGTH]
-
-    elif dataset_id == 2:
-        root_path = "../data/dataset" + str(dataset_id)
-
-        for root, dirs, files in os.walk(root_path):
-            if 'ids' in root:
-                folder_path = root + "/" + "selected"
-                category = os.path.basename(root)
-
-                for sub_root, sub_dirs, sub_files in os.walk(folder_path):
-                    output_file = "../data/full_dataset/dataset" + str(
-                        dataset_id) + "/extracted_data_" + category + ".tsv"
-
-                    # add column names
-                    df_extracted = pd.DataFrame(columns=['Time_Cat', 'Tweet_Id', 'Tweet', 'HashTags'])
-                    df_extracted.to_csv(output_file, sep='\t', mode='a', index=False)
-                    df_extracted = df_extracted[:-1]
-
-                    for file in sub_files:
-                        file_path = folder_path + "/" + file
-                        category = os.path.splitext(file)[0]
-
-                        with open(file_path) as f:
-                            lines = f.readlines()
-
-                            i = 0
-                            for line in lines:
-                                i = i + 1
-                                tweet_id = line
-                                try:
-                                    tweet = api.get_status(tweet_id)
-                                    df_extracted = df_extracted.append(
-                                        {'Time_Cat': category, 'Tweet_Id': tweet_id, 'Tweet': tweet.text,
-                                         'HashTags': get_hashtags(tweet.entities.get('hashtags'))}, ignore_index=True)
-                                except tweepy.TweepError:
-                                    print('Error occurred while retrieving Tweet: ' + str(tweet_id))
-                                    df_extracted = df_extracted.append(
-                                        {'Time_Cat': category, 'Tweet_Id': tweet_id, 'Tweet': 'NA', 'HashTags': 'NA'},
-                                        ignore_index=True)
-
-                                # save to file when ITERATOR_LENGTH found
-                                if i == ITERATOR_LENGTH:
-                                    df_extracted.to_csv(
-                                        output_file, sep='\t', mode='a', index=False, header=False, encoding='utf-8')
-                                    i = 0
-                                    df_extracted = df_extracted[:-ITERATOR_LENGTH]
+        # 450 call per 15 mins
+        # current_time = datetime.datetime.now()
+        # time_diff = current_time - start_time
+        # if i == 450 and time_diff.seconds < TIME_LIMIT:
+        #     print('Sleeping for (seconds) : ', time_diff.seconds)
+        #     time.sleep(TIME_LIMIT - time_diff.seconds)
 
 
 # get hash tag text from tweepy status hashtags
@@ -115,6 +80,22 @@ def get_hashtags(json_array):
     return hashtag_text
 
 
+# get geographic location of this Tweet as reported by the user or client application
+def get_location(coordinates):
+    if coordinates is None:
+        return '_na_,_na_'
+    else:
+        return str(coordinates[0]) + ',' + str(coordinates[1])
+
+
 if __name__ == "__main__":
-    extract_by_id(2)
-    # test()
+    # extract_by_id(2)
+    # tweet = get_tweet_by_id(198808270440378371)
+    # print(tweet.coordinates)
+    # print(get_location(tweet.coordinates))
+    # print(tweet.user)
+    # print(tweet.user.location)
+
+    # get_tweet_by_hashtag("#UCLfinal", "2019-06-01")
+    # get_tweet_by_hashtag("#Barcelona", "2019-10-01")
+    get_tweet_by_hashtag("#BrexitVote ", "2019-10-18")
