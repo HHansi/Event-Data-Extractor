360192c36b94e77bffca7e1bcf4ec4813a6ae619
HHansi <h.hansi1991@gmail.com>
2024-07-04 22:11:39 +0100

    026: Adding youtube data extraction

---
+++ youtube/livechat_extract4.py
@@ -0,0 +1,46 @@
+import pytchat
+import pandas as pd
+import os
+
+
+video_id = 'RTJ3owbsMzM' # https://www.youtube.com/watch?v=RTJ3owbsMzM
+
+if not os.path.exists(video_id):
+    os.makedirs(video_id)
+
+chat = pytchat.create(video_id=video_id)
+
+df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+i = 0
+n = 0
+
+while chat.is_alive():
+    for c in chat.get().sync_items():
+        print(f"{c.id}\t{c.datetime}\t{c.author.name}\t{c.message}")
+        df.loc[i] = [c.id, c.message, c.datetime, c.author.name, c.type]
+        i += 1
+
+        if i==100:
+            print(f'saving iteration {n}')
+            df.to_csv(f"{video_id}/livechat_{n}.csv", index=False, encoding='utf-8')
+            n += 1
+            df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+            i = 0
+
+print()
+
+
+# from pytchat import LiveChat
+# import time
+# chat = LiveChat(video_id = "YEj2RakIf4s")
+#
+# while chat.is_alive():
+#   try:
+#     data = chat.get()
+#     items = data.items
+#     for c in items:
+#         print(f"{c.datetime} [{c.author.name}]- {c.message}")
+#     time.sleep(3)
+#   except KeyboardInterrupt:
+#     chat.terminate()
+#     break
