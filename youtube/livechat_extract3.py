aaa1813185e708f565440c3e05d56090057840c5
HHansi <h.hansi1991@gmail.com>
2024-12-03 19:49:09 +0000

    026: Adding youtube data extraction

--- youtube/livechat_extract3.py
+++ youtube/livechat_extract3.py
@@ -3,28 +3,46 @@ import pandas as pd
 import os
 
 
-video_id = 'cQdOQ3qyShE' # https://www.youtube.com/watch?v=cQdOQ3qyShE&ab_channel=TheTimesandTheSundayTimes
+# video_id = 'cQdOQ3qyShE' # https://www.youtube.com/watch?v=cQdOQ3qyShE&ab_channel=TheTimesandTheSundayTimes
+# video_id = 'iLMKM-9O75M'  # https://www.youtube.com/watch?v=iLMKM-9O75M
+# video_id = 'ZpIcT_u4YU8'  # https://www.youtube.com/watch?v=ZpIcT_u4YU8  - hiru news
+video_id = 'croDm2jrcPk'
 
-if not os.path.exists(video_id):
-    os.makedirs(video_id)
+folder_path = f'south_korea/{video_id}'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
 chat = pytchat.create(video_id=video_id)
 
-df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'channelId', 'channelUrl',
+                           'userType', 'type'])
 i = 0
 n = 0
 
 while chat.is_alive():
     for c in chat.get().sync_items():
-        print(f"{c.id}\t{c.datetime}\t{c.author.name}\t{c.message}")
-        df.loc[i] = [c.id, c.message, c.datetime, c.author.name, c.type]
+        user_type = []
+        if c.author.isChatOwner:
+            user_type.append('ChatOwner')
+        if c.author.isChatSponsor:
+            user_type.append('ChatSponsor')
+        if c.author.isChatModerator:
+            user_type.append('ChatModerator')
+        if c.author.isVerified:
+            user_type.append('Verified')
+
+        print(f"{c.id}\t{c.datetime}\t{c.author.name}\t{c.message}\t{c.author.channelId}")
+        df.loc[i] = [c.id, c.message, c.datetime, c.author.name, c.author.channelId,
+                     c.author.channelUrl, user_type, c.type]
         i += 1
 
         if i==100:
             print(f'saving iteration {n}')
-            df.to_csv(f"{video_id}/livechat_{n}.csv", index=False, encoding='utf-8')
+            df.to_csv(f"{folder_path}/livechat_{n}.csv", index=False, encoding='utf-8')
             n += 1
-            df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+            # df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+            df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'channelId', 'channelUrl',
+                                       'userType', 'type'])
             i = 0
 
 print()
--- youtube/livechat_extract4.py
+++ youtube/livechat_extract4.py
@@ -3,28 +3,47 @@ import pandas as pd
 import os
 
 
-video_id = 'RTJ3owbsMzM' # https://www.youtube.com/watch?v=RTJ3owbsMzM
+# video_id = 'RTJ3owbsMzM' # https://www.youtube.com/watch?v=RTJ3owbsMzM
+# video_id = '756VdjT3KwY' # https://www.youtube.com/watch?v=756VdjT3KwY
+# video_id = 'TIIjtGVl8_A'  # https://www.youtube.com/watch?v=TIIjtGVl8_A  - rupavahini
+video_id = 'SJ2_3d4MGOw'
 
-if not os.path.exists(video_id):
-    os.makedirs(video_id)
+folder_path = f'south_korea/{video_id}'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
 chat = pytchat.create(video_id=video_id)
 
-df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'channelId', 'channelUrl',
+                           'userType', 'type'])
 i = 0
 n = 0
 
+
 while chat.is_alive():
     for c in chat.get().sync_items():
-        print(f"{c.id}\t{c.datetime}\t{c.author.name}\t{c.message}")
-        df.loc[i] = [c.id, c.message, c.datetime, c.author.name, c.type]
+        user_type = []
+        if c.author.isChatOwner:
+            user_type.append('ChatOwner')
+        if c.author.isChatSponsor:
+            user_type.append('ChatSponsor')
+        if c.author.isChatModerator:
+            user_type.append('ChatModerator')
+        if c.author.isVerified:
+            user_type.append('Verified')
+
+        print(f"{c.id}\t{c.datetime}\t{c.author.name}\t{c.message}\t{c.author.channelId}")
+        df.loc[i] = [c.id, c.message, c.datetime, c.author.name, c.author.channelId,
+                     c.author.channelUrl, user_type, c.type]
         i += 1
 
-        if i==100:
+        if i==50:
             print(f'saving iteration {n}')
-            df.to_csv(f"{video_id}/livechat_{n}.csv", index=False, encoding='utf-8')
+            df.to_csv(f"{folder_path}/livechat_{n}.csv", index=False, encoding='utf-8')
             n += 1
-            df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+            # df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'type'])
+            df = pd.DataFrame(columns=['id', 'comment', 'date', 'user_name', 'channelId', 'channelUrl',
+                                       'userType', 'type'])
             i = 0
 
 print()
